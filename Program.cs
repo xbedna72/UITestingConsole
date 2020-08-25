da14e3e915ecde4edbaf08b411b0a09d2fdc2588
mbednarova <marie.bednarova@pa-ats.com>
2020-08-25 14:53:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Management.Automation;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UITestingConsole
+{
+	class Program
+	{
+		public static PowerShell ps;
+		static void Main(string[] args)
+		{
+			Console.WriteLine("UI testing routine___" + DateTime.Now + "__");
+			
+			if(!Constructor()){
+				Console.WriteLine("Unable to open PowerShell command line." +
+				"Press any key to ");
+				Console.ReadLine();
+			}
+			
+			Console.WriteLine(InputLanguage.CurrentInputLanguage.Culture.Name);
+			if (InputLanguage.CurrentInputLanguage.Culture.Name != "en-US")
+			{
+				ChangeSystemLanguage();
+			}
+			Console.WriteLine(InputLanguage.CurrentInputLanguage.Culture.Name);
+			Console.ReadLine();
+		}
+
+		private static bool Constructor()
+		{
+			try
+			{
+				ps = PowerShell.Create();
+				return true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+
+		private static void ChangeSystemLanguage()
+		{
+			ps.AddCommand("powershell - command 'Set-WinUserLanguageList -LanguageList en-US -Force'").Invoke();
+		}
+	}
+}
