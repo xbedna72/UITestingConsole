d3135e4030bd048daadab05d4a964dfb470f1449
mbednarova <marie.bednarova@pa-ats.com>
2020-11-23 14:37:35 +0100

    TestsAreExecuted

--- Program.cs
+++ Program.cs
@@ -23,7 +23,11 @@ namespace UITestingConsole
 			Console.WriteLine("UI testing routine___" + DateTime.Now.ToString() + "___");
 
 			Console.WriteLine("Press any key to close window.....");
-			Console.ReadLine();
+			if(!IsWinAppDriverRunning()){
+				Console.WriteLine("The WinAppDriver is not running. Console executting.");
+				Console.ReadLine();
+				return;
+			}
 
 			Process cmd = new Process();
 			cmd.StartInfo.FileName = @"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\LaunchDevCmd.bat";
@@ -33,14 +37,23 @@ namespace UITestingConsole
 			cmd.StartInfo.UseShellExecute = false;
 			cmd.Start();
 
-			cmd.StandardInput.WriteLine(@"cd C:\\Tools\\UITestingConsole\\TestingFramework\\bin\\Debug &&" +
-			@" vstest.console.exe C:\\Tools\\UITestingConsole\\TestingFramework\\bin\\Debug\\AppiumUITests.dll" +
-			@" /TestAdapterPath:C:\\Tools\\UITestingConsole\\TestingFramework\\bin\\Debug\\netcoreapp3.1" +
-			@" /Logger:trx;LogFileName=C:\\Tools\\Results\\output.trx;verbosity=detailed");
+			cmd.StandardInput.WriteLine(@"cd C:\Tools\XUnitTests\bin\\Debug\netcoreapp3.1 &&" +
+			@" vstest.console.exe XUnitTests.dll /TestAdapterPath:C:\Tools\XUnitTests\bin\Debug\netcoreapp3.1"+ 
+			@"/Logger:trx;LogFileName=C:\Tools\Results\output.trx;verbosity=detailed > C:\Tools\note.txt");
 			cmd.StandardInput.Flush();
 			cmd.StandardInput.Close();
 
 			Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+		}
+
+		static public bool IsWinAppDriverRunning()
+		{
+			Process[] process = Process.GetProcessesByName("WinAppDriver");
+			if (process.Length > 0)
+			{
+				return true;
+			}
+			return false;
 		}
 	}
 }
