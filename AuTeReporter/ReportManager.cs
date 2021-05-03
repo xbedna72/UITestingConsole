f2d81f4f3b5524af312c9081db8d9aea323c5787
Marie Bednářová <xbedna72@vutbr.cz>
2021-05-03 10:53:59 +0200

    update

--- AuTeReporter/ReportManager.cs
+++ AuTeReporter/ReportManager.cs
@@ -2,13 +2,15 @@
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Windows;
 using System;
+using System.IO;
 
 namespace ReportManager
 {
 	//Zdroj: https://github.com/microsoft/WinAppDriver/blob/master/Tests/AbsoluteXPath/DesktopSession.cs
 	public class ReportManager
 	{
-		protected const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
+		private const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
+		private const string WinAppDriverExe = @"C:\Program Files (x86)\Windows Application Driver\WinAppDriver.exe";
 		protected static WindowsDriver<WindowsElement> desktopSessionMain;
 		protected static WindowsDriver<WindowsElement> desktopSession;
 		protected static string application = string.Empty;
@@ -87,8 +89,13 @@ namespace ReportManager
 
 		public static void Initialize(TestContext context)
 		{
+			if (!WinAppDriverControl())
+			{
+				throw new Exception("WinAppDriver path does not exists. See README for more information about needed settings.");
+			}
+
 			application = context.Properties["application"].ToString(); //"Microsoft.WindowsCalculator_8wekyb3d8bbwe!App";
-			winAppDriver = System.Diagnostics.Process.Start(@"C:\Program Files (x86)\Windows Application Driver\WinAppDriver.exe");
+			winAppDriver = System.Diagnostics.Process.Start(WinAppDriverExe);
 			ActualReportModel = new ReportModel(context.FullyQualifiedTestClassName, application);
 			ActualReportModel.testProjectPath = context.Properties["testProjectPath"].ToString();
 			resultDirectory = context.Properties["resultDirectory"].ToString();
@@ -96,11 +103,36 @@ namespace ReportManager
 
 		public static void Initialize(string _application, string _resultDirectory)
 		{
+			if(!File.Exists(_application)){
+				throw new Exception("Path to SUT does not exists.");
+			}
+			if(!WinAppDriverControl()){
+				throw new Exception("WinAppDriver path does not exists. See README for more information about needed settings.");
+			}
+			if(!Directory.Exists(_resultDirectory)){
+				throw new Exception("Path to result directory does not exists.");
+			}
+
 			application = _application;
-			winAppDriver = System.Diagnostics.Process.Start(@"C:\Program Files (x86)\Windows Application Driver\WinAppDriver.exe");
+			winAppDriver = System.Diagnostics.Process.Start(WinAppDriverExe);
 			ActualReportModel = new ReportModel("test", application);
 			ActualReportModel.testProjectPath = "Test";
 			resultDirectory = _resultDirectory;
+		}
+
+		public static void Initialize(string _application)
+		{
+			if (!File.Exists(_application))
+			{
+				throw new Exception("Path to SUT does not exists.");
+			}
+			if (!WinAppDriverControl())
+			{
+				throw new Exception("WinAppDriver path does not exists. See README for more information about needed settings.");
+			}
+
+			application = _application;
+			winAppDriver = System.Diagnostics.Process.Start(WinAppDriverExe);
 		}
 
 		public static void FinalTasks()
@@ -124,6 +156,16 @@ namespace ReportManager
 			{
 				new HtmlCreater(ActualReportModel, resultDirectory);
 			}
+		}
+
+		private static bool WinAppDriverControl()
+		{
+			if (!File.Exists(WinAppDriverExe))
+			{
+				return false;
+			}
+
+			return true;
 		}
 	}
 }
